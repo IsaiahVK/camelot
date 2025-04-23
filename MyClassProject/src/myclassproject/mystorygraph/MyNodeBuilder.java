70874e2887956d3a2be7e4cb47f52e03fba6c90e
Isaiah Kushner <91035100+IsaiahVK@users.noreply.github.com>
2025-04-23 16:46:01 -0500

    Update MyNodeBuilder.java

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeBuilder.java
@@ -42,7 +42,7 @@ public class MyNodeBuilder extends NodeBuilder {
 	@BuilderMethod
 	public void askForAdviceActions() {
 		var node = get(MyNodeLabels.askForAdvice.toString());
-		node.add(new DialogSequence(player, knight, List.of("It takes many years of training to hone your skills young one. If you can prove your worth you can catapault through the ranks."),
+		node.add(new HideDialog()).add(new DialogSequence(player, knight, List.of("It takes many years of training to hone your skills young one. If you can prove your worth you can catapault through the ranks."),
 				List.of("Thank you!")));
 	}
 	//David Paul
@@ -54,7 +54,7 @@ public class MyNodeBuilder extends NodeBuilder {
 	@BuilderMethod
 	public void thankTheKnightActions() {
 		var node = get(MyNodeLabels.thankTheKnight.toString());
-		node.add(new DialogSequence(player, knight, List.of("Oh you're welcome. Now move along."), List.of("Yes sir!")));
+		node.add(new HideDialog()).add(new DialogSequence(player, knight, List.of("Oh you're welcome. Now move along."), List.of("Yes sir!")));
 	}
 
 	//Isaiah Kushner
@@ -63,32 +63,41 @@ public class MyNodeBuilder extends NodeBuilder {
 		var node = get(MyNodeLabels.atTownSquare.toString());
 		node.add(new HideDialog()).add(new DisableInput())
 		.add(new Exit(player, tavernBackDoor, true)).add(new Enter(player, townSquareDoor, true))
-		.add(new EnableInput()).add(new CreateCharacterSequence(friend1)).add(new CreateCharacterSequence(friend2)).add(new SetPosition(friend1, townSquare, "EastEnd"))
-		.add(new SetPosition(friend2, townSquare, "EastEnd")).add(new Face(friend1, player)).add(new Face(friend2, player))
-		.add(new DialogSequence(player, friend1, List.of("Hey! Are you ready for the knight academy?"), List.of("So excited!")));
+		.add(new EnableInput()).add(new CreateCharacterSequence(friend1)).add(new SetPosition(friend1, townSquare, "EastEnd"))
+		.add(new Face(friend1, player));
+	}
+	@BuilderMethod
+	public void friendTalkActions() {
+		var node = get(MyNodeLabels.friendTalk.toString());
+		node.add(new DialogSequence(player, friend1, List.of("Hey! Are you ready for the knight academy?"), List.of("So excited!")));
 	}
 	@BuilderMethod
 	public void friendResponseActions() {
 		var node = get(MyNodeLabels.friendResponse.toString());
-		node.add(new DialogSequence(player, friend1, List.of("What house did you get?"), List.of("Veylthar Manor", "Solara Keep")));
+		node.add(new HideDialog()).add(new DialogSequence(player, friend1, List.of("What house did you get?"), List.of("Veylthar Manor", "Solara Keep")));
 	}
 	@BuilderMethod
 	public void velytharManorActions() {
 		var node = get(MyNodeLabels.velytharManor.toString());
-		node.add(new DialogSequence(player, friend1, List.of("Velythar Manor is the house of magicians and prophets. Harry Houdini founded the house to continue his legacy. We get to be roommates and embark on this journey together!"), List.of("See you later! I'm going to get my key.")));
+		node.add(new HideDialog()).add(new DialogSequence(player, friend1, List.of("Velythar Manor is the house of magicians and prophets. Harry Houdini founded the house to continue his legacy. We get to be roommates and embark on this journey together!"), List.of("See you later! I'm going to get my key.")));
 	}
 	@BuilderMethod
 	public void solaraKeepActions() {
 		var node = get(MyNodeLabels.solaraKeepActions.toString());
-		node.add(new DialogSequence(player, friend1, List.of("Solara Keep is the house of aspiring knights and war mongers. Warriors of the Keep are known to be the protectors of these lands. I'm in Veylthar Manor."), List.of("Oh it sucks so much that we can't be roommates. I guess I will see you later. I'm heading to grab my key.")));
+		node.add(new HideDialog()).add(new DialogSequence(player, friend1, List.of("Solara Keep is the house of aspiring knights and war mongers. Warriors of the Keep are known to be the protectors of these lands. I'm in Veylthar Manor."), List.of("Oh it sucks so much that we can't be roommates. I guess I will see you later. I'm heading to grab my key.")));
 	}
 	@BuilderMethod
 	public void atResidenceOfficeActions() {
 		var node = get(MyNodeLabels.atResidenceOffice.toString());
 		node.add(new HideDialog()).add(new DisableInput()).add(new Exit(player, townSquareDoor, true)).add(new Enter(player, residenceOfficeDoor, true))
 		.add(new EnableInput()).add(new CreateCharacterSequence(oldLady)).add(new SetPosition(oldLady, residenceOffice, "Anvil"))
-		.add(new Face(oldLady, player)).add(new Draw(oldLady, roomKey))
-		.add(new DialogSequence(player, oldLady, List.of("Here is your room key, young lad. Enjoy your stay."), List.of("Thank you, ma' am.")))
+		.add(new Face(oldLady, player)).add(new Draw(oldLady, roomKey));
+	}
+	
+	@BuilderMethod
+	public void ladyTalkActions() {
+		var node = get(MyNodeLabels.ladyTalk.toString());
+		node.add(new DialogSequence(player, oldLady, List.of("Here is your room key, young lad. Enjoy your stay."), List.of("Thank you, ma' am.")))
 		.add(new Draw(player, roomKey));
 	}
 	@BuilderMethod
@@ -104,98 +113,129 @@ public class MyNodeBuilder extends NodeBuilder {
 	}
 	//Davis Voelkel
 	@BuilderMethod
+	public void scrollDoneActions() {
+		var node = get(MyNodeLabels.scrollDone.toString());
+		node.add(new HideNarration()).add(new CreateCharacterSequence(friend2)).add(new SetPosition(friend2, bedroom, "Couch"))
+		.add(new Face(friend2, player));
+	}
+	@BuilderMethod
 	public void wingItActions() {
 		var node = get(MyNodeLabels.wingIt.toString());
-		node.add(new HideNarration()).add(new CreateCharacterSequence(friend2)).add(new SetPosition(friend2, bedroom, "Couch"))
-		.add(new Face(friend2, player)).add(new DialogSequence(player, friend2, List.of("Do you want to eat in the dining hall or go to the club fair?"), List.of("I'm hungry!", "I want to see some clubs.")));
+		node.add(new DialogSequence(player, friend2, List.of("Do you want to eat in the dining hall or go to the club fair?"), List.of("I'm hungry!", "I want to see some clubs.")));
 	}
 	@BuilderMethod
 	public void atDiningHallActions() {
 		var node = get(MyNodeLabels.atDiningHall.toString());
-		node.add(new Exit(player, bedroomDoor, true)).add(new Enter(player, diningHallDoor, true))
-		.add(new SetPosition(dragonfireDumpling,diningTable)).add(new SetPosition(roastedBoar, diningTable)).add(new SetPosition(meatPies, diningTable))
-		.add(new WalkTo(player, diningHall, "Table"))
-		.add(new HideDialog()).add(new NarrationSequence("You arrive at the dining hall and you have never smelled anything so good! You look at the labels for the dishes and decide to eat one."));
+		node.add(new HideDialog()).add(new DisableInput()).add(new Exit(player, bedroomDoor, true)).add(new Enter(player, diningHallDoor, true))
+		.add(new WalkTo(player, diningHall, "Table")).add(new EnableInput())
+		.add(new NarrationSequence("You arrive at the dining hall and you have never smelled anything so good! You look at the labels for the dishes and decide to eat one."));
+	}
+	
+	@BuilderMethod
+	public void chooseEatActions() {
+		var node = get(MyNodeLabels.chooseEat.toString());
+		node.add(new HideNarration()).add(new SetPosition(roastedBoar, diningTable)).add(new SetPosition(meatPies, diningTable)).add(new SetPosition(dragonfireDumpling,diningTable));
 	}
 	@BuilderMethod
 	public void eatDumplingActions() {
 		var node = get(MyNodeLabels.eatDumpling.toString());
-		node.add(new Take(player, dragonfireDumpling)).add(new Drink(player));
+		node.add(new Take(player, dragonfireDumpling)).add(new Drink(player))
+		.add(new NarrationSequence("This is the best food you have ever had. You have never felt more alive and nourished. Your skin glows radiently for a moment. Perhaps this food has magical properties."));
 	}
 	@BuilderMethod
 	public void eatBoarActions() {
 		var node = get(MyNodeLabels.eatBoar.toString());
-		node.add(new Take(player, roastedBoar)).add(new Drink(player));
+		node.add(new Take(player, roastedBoar)).add(new Drink(player))
+		.add(new NarrationSequence("This is the best food you have ever had. You have never felt more alive and nourished. Your skin glows radiently for a moment. Perhaps this food has magical properties."));
 	}
 	@BuilderMethod
 	public void eatPieActions() {
 		var node = get(MyNodeLabels.eatPie.toString()).add(new Drink(player));
-		node.add(new Take(player, meatPies)).add(new Drink(player));
+		node.add(new Take(player, meatPies)).add(new Drink(player)).add(new NarrationSequence("This is the best food you have ever had. You have never felt more alive and nourished. Your skin glows radiently for a moment. Perhaps this food has magical properties."));
 	}
 	@BuilderMethod
-	public void greatTasteActions() {
-		var node = get(MyNodeLabels.greatTaste.toString());
-		node.add(new NarrationSequence("This is the best food you have ever had. You have never felt more alive and nourished. Your skin glows radiently for a moment. Perhaps this food has magical properties."));
+	public void diningToClubActions() {
+		var node = get(MyNodeLabels.diningToClub.toString());
+		node.add(new HideNarration()).add(new DisableInput()).add(new Exit(player, diningHallDoor, true))
+		.add(new Enter(player, clubFairDoor, true))
+		.add(new CreateCharacterSequence(friend3)).add(new SetPosition(friend3, clubFair, "Supplicant"))
+		.add(new SetPosition(friend2, clubFair, "LeftFoyer"))
+		.add(new WalkTo(player, clubFair, "LeftSupplicant")).add(new WalkTo(friend2, clubFair, "LeftSupplicant"))
+		.add(new EnableInput());
 	}
+	@BuilderMethod
+	public void bedroomToClubActions() {
+		var node = get(MyNodeLabels.bedroomToClub.toString());
+		node.add(new HideDialog()).add(new DisableInput()).add(new Exit(player, bedroomDoor, true))
+		.add(new Enter(player, clubFairDoor, true)).add(new CreateCharacterSequence(friend3))
+		.add(new SetPosition(friend3, clubFair, "Supplicant")).add(new SetPosition(friend2, clubFair, "LeftFoyer"))
+		.add(new WalkTo(player, clubFair, "LeftSupplicant")).add(new WalkTo(friend2, clubFair, "LeftSupplicant"))
+		.add(new EnableInput());
+	}
+
 	@BuilderMethod
 	public void atClubFairActions() {
 		var node = get(MyNodeLabels.atClubFair.toString());
-		node.add(new Exit(player, bedroomDoor, true)).add(new Enter(player, clubFairDoor, true))
-		.add(new SetPosition(friend1, clubFair, "Supplicant")).add(new SetPosition(friend2, clubFair, "LeftFoyer"))
-		.add(new WalkTo(player, clubFair, "LeftSupplicant")).add(new WalkTo(friend2, clubFair, "LeftSupplicant"))
-		.add(new CreateCharacterSequence(friend1))
-		.add(new DialogSequence(player, friend1, List.of("Hey! Want to join the archery club? It's a ton of fun!"),
+		node
+		.add(new DialogSequence(player, friend3, List.of("Hey! Want to join the archery club? It's a ton of fun!"),
 				List.of("Sure! I love your recurve bow. Sign me up!")));
 	}
 	//David Paul
-	@BuilderMethod
-	public void clubFairToDiningHallActions() {
-		var node = get(MyNodeLabels.clubFairToDiningHall.toString());
-		node.add(new DisableInput()).add(new Exit(player, clubFairDoor, true)).add(new Enter(player, diningHallDoor, true))
-		.add(new HideDialog()).add(new EnableInput());
-	}
+	
 	@BuilderMethod
 	public void backToBedActions() {
 		var node = get(MyNodeLabels.backToBed.toString());
-		node.add(new Exit(player, diningHallDoor, true)).add(new Enter(player, bedroomDoor, true))
+		node.add(new HideDialog()).add(new DisableInput()).add(new Exit(player, clubFairDoor, true))
+		.add(new Enter(player, bedroomDoor, true))
 		.add(new WalkTo(player, bed))
 		.add(new Sleep(player, bed))
+		.add(new EnableInput())
 		.add(new NarrationSequence("You are dreaming."));
 	}
 	@BuilderMethod
 	public void atHallwayActions() {
 		var node = get(MyNodeLabels.atHallway.toString());
-		node.add(new HideNarration()).add(new DialogSequence(player, player, List.of("Envision your future"), List.of("Blade of Chaos", "Wand of Primordial Passion")))
-		.add(new CreateCharacterSequence(knight))
-		.add(new SetPosition(knight, hallway, "Center")).add(new Face(knight, player));
+		node.add(new HideNarration())
+		.add(new SetPosition(player, hallway, "Stairs"))
+		.add(new DialogSequence(player, player, List.of("Envision your future"), List.of("Blade of Chaos", "Wand of Primordial Passion")))
+		.add(new CreateCharacterSequence(knight2))
+		.add(new SetPosition(knight2, hallway, "Center")).add(new Face(knight2, player));
 	}
 	@BuilderMethod
 	public void bladeOfChaosActions() {
 		var node = get(MyNodeLabels.bladeOfChaos.toString());
-		node.add(new NarrationSequence("Wielded by Kratos Ghost of Sparta, this blade is limitless in its destructive capability. It takes a strong will to put them down when the job is done."));
+		node.add(new HideDialog())
+		.add(new NarrationSequence("Wielded by Kratos Ghost of Sparta, this blade is limitless in its destructive capability. It takes a strong will to put them down when the job is done."));
 	}
 	@BuilderMethod
 	public void wandOfPrimordialPassionActions() {
 		var node = get(MyNodeLabels.wandOfPrimordialPassion.toString());
-		node.add(new NarrationSequence("This legendary wand was used by Merlin himself to purge the dark forces from the lands. This wand allows you to control the four elements of reality as we know it: fire, lightning, water, and earth."));
-	}
+		node.add(new HideDialog())
+		.add(new NarrationSequence("This legendary wand was used by Merlin himself to purge the dark forces from the lands. This wand allows you to control the four elements of reality as we know it: fire, lightning, water, and earth."));
+	} 
 	@BuilderMethod
 	public void useBladeActions() {
 		var node = get(MyNodeLabels.useBlade.toString());
-		node.add(new Draw(player, bladeOfChaos)).add(new WalkTo(player, knight))
-		.add(new Attack(player, knight, true))
-		.add(new Attack(player, knight))
-		.add(new Kneel(knight))
-		.add(new DialogSequence(player, knight, List.of("I yield. You are worthy."), List.of("Thank you for this test.")));
+		node.add(new HideNarration()).add(new Draw(player, bladeOfChaos))
+		.add(new DisableInput()).add(new WalkTo(player, knight2))
+		.add(new Attack(player, knight2, true))
+		.add(new Attack(player, knight2))
+		.add(new Kneel(knight2))
+		.add(new DialogSequence(player, knight2, List.of("I yield. You are worthy."), List.of("Thank you for this test.")));
 	}
 	@BuilderMethod
 	public void useWandActions() {
 		var node = get(MyNodeLabels.useWand.toString());
-		node.add(new Draw(player, wandOfPrimordialPasssion))
-		.add(new Cast(player, knight, Spell.red))
-		.add(new Cast(player, knight, Spell.blue))
-		.add(new Kneel(knight))
-		.add(new DialogSequence(player, knight, List.of("I yield. You are worthy."), List.of("Thank you for this test.")));
+		node.add(new HideNarration()).add(new Draw(player, wandOfPrimordialPasssion))
+		.add(new Cast(player, knight2))
+		.add(new Cast(player, knight2))
+		.add(new Kneel(knight2))
+		.add(new DialogSequence(player, knight2, List.of("I yield. You are worthy."), List.of("Thank you for this test.")));
+	}
+	@BuilderMethod
+	public void endGame() {
+		var node = get(MyNodeLabels.endGame.toString());
+		node.add(new FadeOut()).add(new ShowMenu()); 
 	}
 
 	
