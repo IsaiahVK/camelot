6af47ca26b42bfbea95141fbc088a46ea7ef756a
Isaiah Kushner <91035100+IsaiahVK@users.noreply.github.com>
2025-04-23 16:46:22 -0500

    Update MyNodeLabels.java

--- MyClassProject/src/myclassproject/mystorygraph/MyNodeLabels.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyNodeLabels.java
@@ -2,5 +2,5 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
-	root, atTavern, knightTalk, askForAdvice, showOffSwordsmanship, thankTheKnight, atTownSquare, friendResponse, velytharManor, solaraKeepActions, atResidenceOffice, atRoom, readScroll, atDiningHall, wingIt, eatDumpling, eatBoar, eatPie, atClubFair, greatTaste, clubFairToDiningHall, backToBed, atHallway,bladeOfChaos, wandOfPrimordialPassion, useBlade, useWand, leaveTavern 
+	root, atTavern, knightTalk, askForAdvice, showOffSwordsmanship, thankTheKnight, atTownSquare, friendTalk, friendResponse, velytharManor, solaraKeepActions, atResidenceOffice, atRoom, readScroll, atDiningHall, wingIt, eatDumpling, eatBoar, eatPie, atClubFair, greatTaste, clubFairToDiningHall, backToBed, atHallway,bladeOfChaos, wandOfPrimordialPassion, useBlade, useWand, leaveTavern, ladyTalk, scrollDone, chooseEat, bedroomToClub, diningToClub, endGame,  
 }
