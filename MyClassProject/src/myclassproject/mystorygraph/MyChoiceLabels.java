d8857fa6568051630803df7e6a793fd632e26739
Isaiah Kushner <91035100+IsaiahVK@users.noreply.github.com>
2025-04-23 16:45:21 -0500

    Update MyChoiceLabels.java

--- MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
+++ MyClassProject/src/myclassproject/mystorygraph/MyChoiceLabels.java
@@ -2,6 +2,6 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
-	TalkToKnight, Leave, ShowOffSwordsmanship, PickUpScroll,
+	TalkToKnight, Leave, ShowOffSwordsmanship, PickUpScroll, ThankKnight, TalkToFriend, TalkToLady, WingIt, DragonfireDumpling, RoastedBoar, MeatPies, TalkAtClub,
 	
 }
